49abe4506aa27fe2c44a4e8aa1b465c542556a5f
Denys <speleoden@gmail.com>
2022-06-09 17:55:53 +0200

    move data obtaining and combining logic to backend service

--- dispatcher_plugin_multimessenger/exposer.py
+++ dispatcher_plugin_multimessenger/exposer.py
@@ -1,16 +1,19 @@
 from cdci_data_analysis.analysis.instrument import Instrument
 from cdci_data_analysis.analysis.queries import SourceQuery, InstrumentQuery
 from .queries import MMProductQuery
-
+from .dataserver_dispatcher import MMDataDispatcher
+from . import conf_file
 
 
 def instr_factory():
     return Instrument('multimessenger',
-                      SourceQuery('src_query'),
-                      InstrumentQuery('multi_instrument_query'),
+                      src_query = SourceQuery('src_query'),
+                      instrumet_query = InstrumentQuery('multi_instrument_query'),
+                      data_serve_conf_file=conf_file,
                       product_queries_list=[MMProductQuery('mm_prod_query')],
                       query_dictionary={'multimessenger': 'mm_prod_query'},
-                      asynch=False, 
+                      asynch=True, 
+                      data_server_query_class=MMDataDispatcher,
                       )
 
 instr_factory_list = [instr_factory]
--- dispatcher_plugin_multimessenger/queries.py
+++ dispatcher_plugin_multimessenger/queries.py
@@ -1,18 +1,6 @@
 from tokenize import Name
 from cdci_data_analysis.analysis.queries import ProductQuery, QueryOutput
 from cdci_data_analysis.analysis.parameters import Name
-import json
-import os
-from collections import OrderedDict
-
-from oda_api.api import DispatcherAPI
-from bokeh.io import output_notebook
-from bokeh.plotting import figure, show
-from bokeh.models import Whisker, ColumnDataSource
-from bokeh.layouts import gridplot
-
-from astropy.time import Time
-from astropy import units as u
 
 from .products import MultiInstrumentProduct
 
@@ -20,26 +8,25 @@ class MMProductQuery(ProductQuery):
     def __init__(self, name):
         anal_par_csv = Name(name='anal_par_csv')
         super().__init__(name, parameters_list = [anal_par_csv])
-    
-    # TODO: can be defined in MMDataDispatcher, what's better?
-    def test_communication(self, *args, **kwargs):
-        query_out = QueryOutput()
-        query_out.set_done()
-        return query_out
-    
-    def test_has_products(self, *args, **kwargs):
-        # TODO: actual testing for dirs here?
-        query_out = QueryOutput()
-        query_out.set_products(['input_prod_list'], [[]])
-        query_out.set_done()
-        return query_out    
-    
+   
+ 
     def get_data_server_query(self, instrument, config=None, **kwargs):
         param_dict = {'anal_par_csv': instrument.get_par_by_name('anal_par_csv')}
-        return MMDataDispatcher(param_dict) 
+        return instrument.data_server_query_class(instrument=instrument,
+                                                  config=config,
+                                                  param_dict=param_dict,
+                                                  task='/api/v1.0/get/prodcombine')
+
     
     def build_product_list(self, instrument, res, out_dir, api=False):
-        prod_list = [MultiInstrumentProduct(res)]
+        prod_list = []
+        if out_dir is None:
+            out_dir = './'
+        if 'output' in res.json().keys(): # in synchronous mode
+            _o_dict = res.json() 
+        else:
+            _o_dict = res.json()['data']
+        prod_list = [MultiInstrumentProduct(_o_dict)]
         return prod_list
     
     def process_product_method(self, instrument, prod_list, api=False):
@@ -47,7 +34,7 @@ class MMProductQuery(ProductQuery):
         image_prod  = prod_list.prod_list[0]
 
         if api is True:
-            raise NotImplemented
+            raise NotImplementedError
         else:
             plot_dict = {'image': image_prod.get_plot()}
             #image_prod.write() 
@@ -59,90 +46,4 @@ class MMProductQuery(ProductQuery):
             query_out.prod_dictionary['prod_process_message'] = ''
 
         return query_out
-    
-class MMDataDispatcher:
-    def __init__(self, param_dict) -> None:
-        pars = param_dict['anal_par_csv'].value.split(';')
-        self.products_api_params = [self.get_params_from_string(x) for x in pars]
-    
-    def run_query(self, 
-                  call_back_url=None,
-                  run_asynch = False,
-                  logger=None):
-        
-        # FIXME: hardcoded dispatcher self-address
-        disp = DispatcherAPI(url='http://127.0.0.1:8000', instrument='mock')
-        
-        res = [disp.get_product(**par_dict) for par_dict in self.products_api_params]
-                
-        query_out = QueryOutput()
-        query_out.set_done(job_status='done')
-        return res, query_out
-    
-    @staticmethod
-    def get_params_from_string(pars):
-        query_dict = json.loads(pars)
-        query_dict = OrderedDict(sorted(query_dict.items()))
-        _skip_list_ = ['job_id', 'query_status',
-                       'session_id', 'use_resolver[local]', 'use_scws']
-
-        _alias_dict = {}
-        _alias_dict['product_type'] = 'product'
-        _alias_dict['query_type'] = 'product_type'
-
-        _api_dict = {}
-        for k in query_dict.keys():
-            if k not in _skip_list_:
-
-                if k in _alias_dict.keys():
-                    n = _alias_dict[k]
-
-                else:
-                    n = k
-
-                if query_dict[k] is not None:
-                    _api_dict[n] = query_dict[k]
-        
-        return _api_dict
-    
-    # @staticmethod
-    # def get_params_from_dir(path):
-    #     try:
-    #         with open(os.path.join(path, 'analysis_parameters.json')) as fd:
-    #             query_dict = json.load(fd)
-    #     except FileNotFoundError:
-    #         with open(os.path.join(path+'_aliased', 'analysis_parameters.json')) as fd:
-    #             query_dict = json.load(fd)
-                
-    #     # TODO: adapted from oda_api, some code duplication
-    #     query_dict = OrderedDict(sorted(query_dict.items()))
-
-    #     _skip_list_ = ['job_id', 'query_status',
-    #                    'session_id', 'use_resolver[local]', 'use_scws']
-
-    #     _alias_dict = {}
-    #     _alias_dict['product_type'] = 'product'
-    #     _alias_dict['query_type'] = 'product_type'
-
-    #     _api_dict = {}
-    #     for k in query_dict.keys():
-    #         if k not in _skip_list_:
-
-    #             if k in _alias_dict.keys():
-    #                 n = _alias_dict[k]
-
-    #             else:
-    #                 n = k
-
-    #             if query_dict[k] is not None:
-    #                 _api_dict[n] = query_dict[k]
-        
-    #     return _api_dict
-    
-    # def get_products_api_params(self):
-    #     self.products_api_params = []
-    #     for dirid in self.ids:
-    #         self.products_api_params.append(self.get_params_from_dir(dirid))
-            
-    
     
