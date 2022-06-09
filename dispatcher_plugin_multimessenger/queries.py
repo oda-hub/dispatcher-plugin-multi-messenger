9e974fcb0d11796f76d16d8985a7bed60caf2857
Denys <speleoden@gmail.com>
2022-06-09 10:29:20 +0200

    use parameter dict strings instead of ids

--- dispatcher_plugin_multimessenger/queries.py
+++ dispatcher_plugin_multimessenger/queries.py
@@ -18,8 +18,8 @@ from .products import MultiInstrumentProduct
 
 class MMProductQuery(ProductQuery):
     def __init__(self, name):
-        ids_csv = Name(name='ids_csv')
-        super().__init__(name, parameters_list = [ids_csv])
+        anal_par_csv = Name(name='anal_par_csv')
+        super().__init__(name, parameters_list = [anal_par_csv])
     
     # TODO: can be defined in MMDataDispatcher, what's better?
     def test_communication(self, *args, **kwargs):
@@ -35,7 +35,7 @@ class MMProductQuery(ProductQuery):
         return query_out    
     
     def get_data_server_query(self, instrument, config=None, **kwargs):
-        param_dict = {'ids_csv': instrument.get_par_by_name('ids_csv')}
+        param_dict = {'anal_par_csv': instrument.get_par_by_name('anal_par_csv')}
         return MMDataDispatcher(param_dict) 
     
     def build_product_list(self, instrument, res, out_dir, api=False):
@@ -62,7 +62,8 @@ class MMProductQuery(ProductQuery):
     
 class MMDataDispatcher:
     def __init__(self, param_dict) -> None:
-        self.ids = param_dict['ids_csv'].value.split(',')
+        pars = param_dict['anal_par_csv'].value.split(';')
+        self.products_api_params = [self.get_params_from_string(x) for x in pars]
     
     def run_query(self, 
                   call_back_url=None,
@@ -72,8 +73,6 @@ class MMDataDispatcher:
         # FIXME: hardcoded dispatcher self-address
         disp = DispatcherAPI(url='http://127.0.0.1:8000', instrument='mock')
         
-        self.get_products_api_params()
-        
         res = [disp.get_product(**par_dict) for par_dict in self.products_api_params]
                 
         query_out = QueryOutput()
@@ -81,17 +80,9 @@ class MMDataDispatcher:
         return res, query_out
     
     @staticmethod
-    def get_params_from_dir(path):
-        try:
-            with open(os.path.join(path, 'analysis_parameters.json')) as fd:
-                query_dict = json.load(fd)
-        except FileNotFoundError:
-            with open(os.path.join(path+'_aliased', 'analysis_parameters.json')) as fd:
-                query_dict = json.load(fd)
-                
-        # TODO: adapted from oda_api, some code duplication
+    def get_params_from_string(pars):
+        query_dict = json.loads(pars)
         query_dict = OrderedDict(sorted(query_dict.items()))
-
         _skip_list_ = ['job_id', 'query_status',
                        'session_id', 'use_resolver[local]', 'use_scws']
 
@@ -114,10 +105,44 @@ class MMDataDispatcher:
         
         return _api_dict
     
-    def get_products_api_params(self):
-        self.products_api_params = []
-        for dirid in self.ids:
-            self.products_api_params.append(self.get_params_from_dir(dirid))
+    # @staticmethod
+    # def get_params_from_dir(path):
+    #     try:
+    #         with open(os.path.join(path, 'analysis_parameters.json')) as fd:
+    #             query_dict = json.load(fd)
+    #     except FileNotFoundError:
+    #         with open(os.path.join(path+'_aliased', 'analysis_parameters.json')) as fd:
+    #             query_dict = json.load(fd)
+                
+    #     # TODO: adapted from oda_api, some code duplication
+    #     query_dict = OrderedDict(sorted(query_dict.items()))
+
+    #     _skip_list_ = ['job_id', 'query_status',
+    #                    'session_id', 'use_resolver[local]', 'use_scws']
+
+    #     _alias_dict = {}
+    #     _alias_dict['product_type'] = 'product'
+    #     _alias_dict['query_type'] = 'product_type'
+
+    #     _api_dict = {}
+    #     for k in query_dict.keys():
+    #         if k not in _skip_list_:
+
+    #             if k in _alias_dict.keys():
+    #                 n = _alias_dict[k]
+
+    #             else:
+    #                 n = k
+
+    #             if query_dict[k] is not None:
+    #                 _api_dict[n] = query_dict[k]
+        
+    #     return _api_dict
+    
+    # def get_products_api_params(self):
+    #     self.products_api_params = []
+    #     for dirid in self.ids:
+    #         self.products_api_params.append(self.get_params_from_dir(dirid))
             
     
     
